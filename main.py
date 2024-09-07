f47c645124eebe008ffeb5e3cdde65ecb5527907
makingcoolcode1 <zachgedge@gmail.com>
2024-09-07 13:30:46 -0400

    updating syntax and adding URL link in search retuns

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 
 import requests
 
-print("\n**WELCOME TO THE NEWS APP**")
-print("\nTo use this program, please sign up for a free API key at openewsapi.org")
-print("Type 'exit' at anytime to close the program")
+print("\n**Welcome to the news search tool**")
+print("\nYou will require an API key from newsapi.org to access the app")
 
-
-def validate_api_key(api_key):
+def verify_api_key(api_key):
 
     test_url = f"https://newsapi.org/v2/everything?q=baltimore&apiKey={api_key}"
 
@@ -14,58 +12,58 @@ def validate_api_key(api_key):
 
     return test_request.status_code == 200
 
-def get_news(news_search):
+def get_news(news_input):
 
-    url = f"https://newsapi.org/v2/everything?q={news_search}&apiKey={api_key}"
+    URL = f"https://newsapi.org/v2/everything?q={news_input}&apiKey={api_key}"
 
-    json = requests.get(url).json()
+    json = requests.get(URL).json()
 
     title_0 = json["articles"][0]["title"]
     description_0 = json["articles"][0]["description"]
+    link_0 = json["articles"][0]["url"]
 
     title_1 = json["articles"][1]["title"]
-    description_1 = json['articles'][1]["description"]
+    description_1 = json["articles"][1]["description"]
+    link_1 = json["articles"][1]["url"]
 
     title_2 = json["articles"][2]["title"]
     description_2 = json["articles"][2]["description"]
+    link_2 = json["articles"][2]["url"]
 
-    print("Title:", title_0)
+    print("\nTitle: ", title_0)
     print("\n", description_0)
-    print("\nTitle",title_1)
+    print("\nRead More: ", link_0)
+    print("\nTitle: ", title_1)
     print("\n", description_1)
-    print("\nTitle:", title_2)
+    print("\nRead More: ", link_1)
+    print("\nTitle: ", title_2)
     print("\n", description_2)
-    
+    print("\nRead More: ", link_2)
+
 
 
 while True:
-
     api_key = input("\nPlease enter your API key: ")
 
-    
     if api_key.lower() == "exit":
-        print("Exiting Program....")
+        print("Exiting News App....")
         break
 
-
-    if (validate_api_key(api_key)):
-        print("\nAPI KEY VALID.....Proceeding to News App!\n")
+    if verify_api_key(api_key):
+        print("\nAPI KEY VALIDATED!")
         break
     else:
-        print("ERROR! Failed to validate API key. Please check your key input")
+        print("\nERROR! Failed to validate API key. Please check your key")
 
 
 while True:
 
-    news_search = input("\nPlease enter a city to search ")
+    news_input = input("\nEnter a search query: ")
 
-    if news_search.lower() == "exit":
-        print("Exiting Program....")
+    if news_input.lower() == "exit":
         break
-
+    
     try:
-        get_news(news_search)
+        get_news(news_input)
     except:
-        print("No News Articles Found, Please enter another search query.")
-
-
+        print("\nNo News Articles Found, Please try another search")
