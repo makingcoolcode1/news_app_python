a17a18dd767df116b02d33246df20b2ae7e20337
makingcoolcode1 <zachgedge@gmail.com>
2024-09-08 14:03:26 -0400

    fixing bug where code does not break if user types exit during api prompt

---
+++ main.py
@@ -0,0 +1,70 @@
+
+import requests
+
+print("\n**Welcome to the news search tool**")
+print("\nYou will require an API key from newsapi.org to access the app")
+
+def verify_api_key(api_key):
+
+    test_url = f"https://newsapi.org/v2/everything?q=baltimore&apiKey={api_key}"
+
+    test_request = requests.get(test_url)
+
+    return test_request.status_code == 200
+
+def get_news(news_input):
+
+    URL = f"https://newsapi.org/v2/everything?q={news_input}&apiKey={api_key}"
+
+    json = requests.get(URL).json()
+
+    title_0 = json["articles"][0]["title"]
+    description_0 = json["articles"][0]["description"]
+    link_0 = json["articles"][0]["url"]
+
+    title_1 = json["articles"][1]["title"]
+    description_1 = json["articles"][1]["description"]
+    link_1 = json["articles"][1]["url"]
+
+    title_2 = json["articles"][2]["title"]
+    description_2 = json["articles"][2]["description"]
+    link_2 = json["articles"][2]["url"]
+
+    print("\nTitle: ", title_0)
+    print("\n", description_0)
+    print("\nRead More: ", link_0)
+    print("\nTitle: ", title_1)
+    print("\n", description_1)
+    print("\nRead More: ", link_1)
+    print("\nTitle: ", title_2)
+    print("\n", description_2)
+    print("\nRead More: ", link_2)
+
+
+
+while True:
+    api_key = input("\nPlease enter your API key: ")
+
+    if api_key.lower() == "exit":
+        print("Exiting News App....")
+        exit_program  = True
+        break
+
+    if verify_api_key(api_key):
+        print("\nAPI KEY VALIDATED!")
+        break
+    else:
+        print("\nERROR! Failed to validate API key. Please check your key")
+
+if not exit_program: 
+    while not exit_program:
+
+        news_input = input("\nEnter a search query: ")
+
+        if news_input.lower() == "exit":
+            break
+        
+        try:
+            get_news(news_input)
+        except:
+            print("\nNo News Articles Found, Please try another search")
